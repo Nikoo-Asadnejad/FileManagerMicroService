9c1b445594d84ec0e93b4b8c8dc80e4797a5e26c
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-07-22 17:43:41 +0430

    Chores : add test project and test packages

--- FileManager/FileManager/Dtos/UploadDtos.cs
+++ FileManager/FileManager/Dtos/UploadDtos.cs
@@ -1,5 +1,5 @@
 namespace FileManager.Dtos;
 public record UploadInputModel(IFormFile File , string Title);
-public record UploadReturnModel(Guid Id);
+public record UploadReturnModel(Guid FileId);
 
 
